bf438ba357d3ff2405bd2a3fb935878822fe9874
Reymundo Tenorio <Reymundo Tenorio>
2017-01-11 00:28:32 -0600

    Se ha creado el proyecto para comprobar conexión a internet

---
+++ CheckInternetConnection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace CheckInternetConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CheckForInternetConnection())
+            {
+                Console.WriteLine("Conexión a internet establecida");
+            }
+            else
+            {
+
+                Console.WriteLine("Sin conexión a internet");
+            }
+
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadLine();
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                Stream Stream = client.OpenRead("http://www.google.com");
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
